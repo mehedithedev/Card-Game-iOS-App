3f8705518a37504e24636a4848946f5f5ceaf4ed
mehedithedev <mehedi.developer@icloud.com>
2024-10-21 15:35:07 -0600

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Mehedi Hasan on 2024-10-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ war-challengeTests/war_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeTests.swift
+//  war-challengeTests
+//
+//  Created by Mehedi Hasan on 2024-10-21.
+//
+
+import Testing
+@testable import war_challenge
+
+struct war_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
